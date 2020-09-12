afb873b50e9bc1adae5b8217752016025b13c9e2
Satrat <sara.adkins65@gmail.com>
2020-09-12 15:59:48 -0400

    store GUI info in data classes

--- config_app/pedal_config.py
+++ config_app/pedal_config.py
@@ -1,8 +1,25 @@
 import sys
 import serial
+from dataclasses import dataclass
 from cobs import cobs
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QComboBox, QGridLayout, QCheckBox
 from PyQt5.QtCore import Qt
+
+@dataclass
+class MidiMessage:
+    midi_type: int
+    channel: int
+    data_1: int
+    data_2: int = 0
+
+@dataclass
+class StompConfigData:
+    state: int
+    stomp_id: int
+    cycle_dir: int
+    cycle_len: int
+    midi_msg_press: MidiMessage
+    midi_msg_release: MidiMessage = None
 
 class MidiMessageConfigOptions(QWidget):
     def __init__(self):
@@ -17,7 +34,7 @@ class MidiMessageConfigOptions(QWidget):
 
         self.midi_channel_label = QLabel("Channel: ")
         self.midi_channel = QComboBox()
-        for i in range(16):
+        for i in range(1,17):
             self.midi_channel.addItem(str(i))
 
         self.midi_data1_label = QLabel("Data Byte 1: ")
@@ -27,6 +44,7 @@ class MidiMessageConfigOptions(QWidget):
         for i in range(128):
             self.midi_data1.addItem(str(i))
             self.midi_data2.addItem(str(i))
+        self.midi_msg_type.currentIndexChanged.connect(self.on_msg_type_changed)
 
         self.layout = QGridLayout()
         self.layout.addWidget(self.midi_msg_type_label,0,0)
@@ -39,17 +57,45 @@ class MidiMessageConfigOptions(QWidget):
         self.layout.addWidget(self.midi_data2,3,1)
         self.setLayout(self.layout)
 
-    def disableAll(self):
+    def on_msg_type_changed(self):
+        curr_idx = self.midi_msg_type.currentIndex()
+        if curr_idx == 1 or curr_idx == 3:
+            self.midi_data2.setEnabled(False)
+            self.midi_data2.setCurrentIndex(0)
+        else:
+            self.midi_data2.setEnabled(True)
+
+    def disable_all(self):
         self.midi_msg_type.setEnabled(False)
         self.midi_channel.setEnabled(False)
         self.midi_data1.setEnabled(False)
         self.midi_data2.setEnabled(False)
 
-    def enableAll(self):
+    def enable_all(self):
         self.midi_msg_type.setEnabled(True)
         self.midi_channel.setEnabled(True)
         self.midi_data1.setEnabled(True)
         self.midi_data2.setEnabled(True)
+
+    def get_current_midi_message(self):
+        msg_type = self.midi_msg_type.currentText()
+        if msg_type == "NOTE_ON":
+            msg_type = 8
+        elif msg_type == "NOTE_OFF":
+            msg_type = 9
+        elif msg_type == "CONTROL_CHANGE":
+            msg_type = 11
+        else: #PROGRAM_CHANGE
+            msg_type = 12
+        
+        channel = int(self.midi_channel.currentText())
+        data1 = int(self.midi_data1.currentText())
+        data2 = int(self.midi_data2.currentText())
+
+        midi_msg = MidiMessage(msg_type, channel, data1, data2)
+        return midi_msg
+
+        
 
 class StompConfigOptions(QWidget):
     def __init__(self, stomp_id):
@@ -57,7 +103,7 @@ class StompConfigOptions(QWidget):
         self.stomp_id = stomp_id
 
         self.title = QLabel()
-        self.title.setText(f"Stomp Box {stomp_id}")
+        self.title.setText(f"Stomp Box {stomp_id + 1}")
         self.title.setAlignment(Qt.AlignCenter)
         self.title.setStyleSheet("font-weight: bold; padding-bottom: 10px;")
 
@@ -79,7 +125,7 @@ class StompConfigOptions(QWidget):
         self.release_label = QLabel("Release Message")
         self.release_label.setAlignment(Qt.AlignCenter)
         self.release_midi_options = MidiMessageConfigOptions()
-        self.release_midi_options.disableAll()
+        self.release_midi_options.disable_all()
         
         self.layout = QGridLayout()
         self.layout.addWidget(self.title,0,0,1,4)
@@ -96,11 +142,11 @@ class StompConfigOptions(QWidget):
 
     def on_release_trigger_toggled(self):
         if self.trigger_release.isChecked():
-            self.release_midi_options.enableAll()
+            self.release_midi_options.enable_all()
             self.cycle_press.setChecked(False)
             self.cycle_press.setEnabled(False)
         else:
-            self.release_midi_options.disableAll()
+            self.release_midi_options.disable_all()
             self.cycle_press.setEnabled(True)
 
     def on_cycle_press_toggled(self):
@@ -108,6 +154,22 @@ class StompConfigOptions(QWidget):
             self.cycle_length.setEnabled(True)
         else:
             self.cycle_length.setEnabled(False)
+            self.cycle_length.setCurrentIndex(0)
+
+    def get_current_config_data(self):
+        state = int(self.trigger_release.isChecked())
+        stomp_id = self.stomp_id
+        cycle_dir = int(self.cycle_press.isChecked())
+        cycle_len = int(self.cycle_length.currentText())
+        midi_msg_press = self.press_midi_options.get_current_midi_message()
+
+        if state:
+            midi_msg_release = self.release_midi_options.get_current_midi_message()
+        else:
+            midi_msg_release = None
+
+        config_msg = StompConfigData(state, stomp_id, cycle_dir, cycle_len, midi_msg_press, midi_msg_release)
+        return config_msg
 
 class PedalConfigOptions(QWidget):
     def __init__(self, num_stomps):
@@ -116,7 +178,7 @@ class PedalConfigOptions(QWidget):
         self.num_stomps = num_stomps
         self.stomps = []
         for i in range(self.num_stomps):
-            self.stomps.append(StompConfigOptions(i+1))
+            self.stomps.append(StompConfigOptions(i))
 
         self.layout = QHBoxLayout()
         for stomp in self.stomps:
@@ -203,10 +265,12 @@ class MainWindow(QMainWindow):
         self.serial.close()
 
     def onButtonClicked(self):
-        self.serialOpen()
-        msg = self.encodeMessage()
-        self.sendCobsMessage(msg)
-        self.serialClose()
+        #self.serialOpen()
+        #msg = self.encodeMessage()
+        #self.sendCobsMessage(msg)
+        #self.serialClose()
+        for stomp in self.pedal_config.stomps:
+            print(stomp.get_current_config_data())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
